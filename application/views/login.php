c4aac9b276e862a9b15e846758f76abaf5b85a3b
ashenmalshika <94889701+ashenmalshika@users.noreply.github.com>
2023-05-04 02:06:59 +0530

    login page sucessfully implemented

--- application/views/login.php
+++ application/views/login.php
@@ -1,40 +1,56 @@
-<!DOCTYPE html>
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <title>Login Page</title>
     <style>
-		<?php include 'login.css'; ?>
-	</style>
+        <?php include 'logins.css'; ?>
+    </style>
 </head>
-
 <body>
-    <div class="center">
-        <div class="container">
-            <div class="text">
-                Login
-            </div>
-            <img id='image' src="<?php echo base_url('assets/images/admin1.jpg'); ?>" alt="My Image">
-            <?php echo form_open('Login/LoginUser'); ?>
-                <div class="data">
-                    <input type="text" name="username" placeholder="Username">
-                </div>
-                <div class="data">
-                    <input type="password" name="password" class="password" placeholder="Password">
-                </div>
-                <?php echo validation_errors(); ?>
-                <?php if($this->session->flashdata('errormsg')){
-                    echo "<h3>".$this->session->flashdata('errormsg')."</h3>";
-                }
-                ?>
-                <div class="btn">
-                    <div class="inner"></div>
-                    <button type="submit">login</button>
-                </div>
-            <?php form_close(); ?>
-        </div>
+    <div class="back-image">
+       <img src="<?php echo base_url('assets/images/bk.png'); ?>" alt="background image">
     </div>
-</body>
+    <section class="left">
+        <section class="side login">  
+            <div class="box">
+                <img id="logo" src="<?php echo base_url('assets/images/logo.svg'); ?>" alt=" " width="140px">
+            </div>
+            <p class="title">Employees Attendance Tracking System</p>
+            <br>
+            <img src="<?php echo base_url('assets/images/mobile.png'); ?>" alt=" ">
+            
+        </section>
+    </section>
 
+    <section class="right">
+        <section class="main login">
+            <div class="container">               
+                <p class="title">Login</p>
+                <?php echo form_open('Login/LoginUser'); ?>
+                    <div class="form-control">
+                        <input type="text" name="username" placeholder="Username">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="form-control">
+                        <input type="password" name="password" placeholder="Password">
+                        <i class="fas fa-unlock"></i>
+                    </div>
+                    <?php echo validation_errors(); ?>
+                        <?php if($this->session->flashdata('errormsg')){
+                            echo "<h3>".$this->session->flashdata('errormsg')."</h3>";
+                            }
+                        ?>
+                    <button type="submit" class="submit">Login</button>
+                <?php form_close(); ?>
+            </div>
+        </section>
+
+    </section>
+    
+</body>
 </html>
+
+
