2b5805fb545867ebd5b6154a0a0e0e19f4615f0e
ashenmalshika <94889701+ashenmalshika@users.noreply.github.com>
2023-05-13 22:30:15 +0530

    password encryption done

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -4,9 +4,10 @@ class user_model extends CI_Model{
     function getdata(){
         $username=$this->input->post('username');
         $password=$this->input->post('password');
+        $encrypt = sha1($password);
 
         $this->db->where('username',$username);
-        $this->db->where('password',$password);
+        $this->db->where('password',$encrypt);
 
         $respond=$this->db->get('admintable');                               //check in users table
         if($respond->num_rows()==1){
