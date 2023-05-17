ae967f8e1f19407ac8fc8aa6e8d2fb9ed1dcc061
ashenmalshika <94889701+ashenmalshika@users.noreply.github.com>
2023-05-18 03:21:48 +0530

    new changes done

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -4,10 +4,10 @@ class user_model extends CI_Model{
     function getdata(){
         $username=$this->input->post('username');
         $password=$this->input->post('password');
-        $encrypt = sha1($password);
+
 
         $this->db->where('username',$username);
-        $this->db->where('password',$encrypt);
+        $this->db->where('password',$password);
 
         $respond=$this->db->get('admintable');                               //check in users table
         if($respond->num_rows()==1){
